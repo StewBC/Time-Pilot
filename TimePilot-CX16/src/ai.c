57df777df1acd8f1b752b9897923508e02488df0
Stefan Wessels <swessels@email.com>
2024-03-11 21:01:21 -0700

    Kill player bullets when they leave the screen

--- TimePilot-CX16/src/ai.c
+++ TimePilot-CX16/src/ai.c
@@ -540,7 +540,10 @@ void aiLevelBoss(int8_t X) {
 //-----------------------------------------------------------------------------
 void aiNonWrapping(int8_t X) {
     if((activeFlags[X] & ACTIVEFLAGS_CLIPMASK) == ACTIVEFLAGS_CLIPMASK) {
-        if(activeLayer[X] == LAYER_ENEMY_BULLETS || activeLayer[X] == LAYER_ENEMY_SPACEBULLETS) {
+        if(activeLayer[X] == LAYER_PLAYER_BULLETS) {
+            activeFlags[X] |= ACTIVEFLAGS_REMOVE;
+            return;
+        } else if(activeLayer[X] == LAYER_ENEMY_BULLETS || activeLayer[X] == LAYER_ENEMY_SPACEBULLETS) {
             activeFlags[X] |= ACTIVEFLAGS_REMOVE;
             if(activeFlags[X] & (ACTIVEFLAGS_MULTIPURPOSE | ACTIVEFLAGS_TRACKED)) {
                 // Horizontal flyer bullet
